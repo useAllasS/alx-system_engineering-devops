19fc3766e29060435c59930e975233ace54a4ae4
useAllas <nitayoussef3@gmail.com>
2024-03-22 16:22:46 -0700

    error1000

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,6 @@
-#instal flask 1-install_a_package.pp
-
-exec { 'python-installed':
-  command => '/usr/bin/which python3',
-}
-
-exec { 'pip3 install flask':
-  require => Exec['python-installed'],
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  command => '/usr/bin/pip3 install flask==2.1.0',
+# install flask from pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
 
