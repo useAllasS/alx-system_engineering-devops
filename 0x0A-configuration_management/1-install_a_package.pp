a144fd7bf58c89083b0aaae5e43cab53425b335b
useAllas <nitayoussef3@gmail.com>
2024-03-22 16:06:29 -0700

    error3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,12 @@
-#Install flask version 2.1.0
+#instal flask 1-install_a_package.pp
 
-
-exec {'pip3 install flask':
-require => Exec['python-installed'],
-command => '/usr/bin/pip3 install flask==2.1.0'
+exec { 'python-installed':
+  command => '/usr/bin/which python3',
 }
 
-exec {'python-installed':
-command => '/usr/bin/which python3'
+exec { 'pip3 install flask':
+  require => Exec['python-installed'],
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  command => '/usr/bin/pip3 install flask==2.1.0',
 }
 
